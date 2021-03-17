a6f10f3c5c05fe476c4a084e0dc22aad5d65362c
Mrsmithz <Dechapon.n@hotmail.com>
2021-03-17 17:37:05 +0700

    update

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ const cors = require('cors')
 const PORT = 25800
 app.use(cors())
 app.use((req, res, next) => {
-    console.log(`requested at ${req.headers.host} method ${req.method}`)
+    console.log(`requested at ${req.url} method ${req.method}`)
     next()
 })
 app.all('/api/*', (req, res, next) => {
     console.log('API KEY CHECKER')
 })
 app.use('/', (req, res) => {
-    res.send(`<h1>Hello</h1>`)
+    res.send('Hello')
 })
 
 
