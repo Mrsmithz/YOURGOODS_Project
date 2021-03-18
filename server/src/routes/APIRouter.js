a0866842b64c1013eb4db34b401e7effe754fd0c
Mrsmithz <Dechapon.n@hotmail.com>
2021-03-19 04:42:43 +0700

    update router and add middleware

--- server/src/routes/APIRouter.js
+++ server/src/routes/APIRouter.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const router = express.Router()
-
-router.get('/', (req, res) => {
-    res.send('first test api')
+const User = require('../model/User')
+const RegisterController = require('../controller/RegisterController')
+router.post('/login', async (req, res) => {
+    var user = new User(null, req.body.username, req.body.password)
+    try{
+        await user.getUserId()
+        var json_string = JSON.stringify(await user.login())
+        var data = JSON.parse(json_string)[0]
+        console.log(data)
+    }
+    catch(err) {
+        console.log(err)
+    }
 })
-router.get('/test', (req, res) => {
-    res.send('tester')
-})
+router.post('/register/customer', RegisterController.customerRegister)
+router.post('/register/staff/supervisor', RegisterController.supervisorRegister)
+router.post('/register/staff/order_manager', RegisterController.orderManagerRegister)
+router.post('/register/staff/messenger', RegisterController.messengerRegister)
 module.exports = router
