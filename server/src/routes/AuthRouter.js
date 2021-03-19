cc4cbe3534407ee3b04f6fefef986deb43a5cf99
Mrsmithz <Dechapon.n@hotmail.com>
2021-03-20 05:22:18 +0700

    rename from APIRouter

---
+++ server/src/routes/AuthRouter.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../model/User')
+const RegisterController = require('../controller/RegisterController')
+const LoginController = require('../controller/LoginController')
+router.post('/login', LoginController.checkLoginRoute)
+router.post('/login/customer', LoginController.customerLogin)
+router.post('/login/supervisor', LoginController.supervisorLogin)
+router.post('/login/order_manager', LoginController.orderManagerLogin)
+router.post('/login/messenger', LoginController.messengerLogin)
+
+router.post('/register', RegisterController.checkRegisterRoute)
+router.post('/register/customer', RegisterController.customerRegister)
+router.post('/register/supervisor', RegisterController.supervisorRegister)
+router.post('/register/order_manager', RegisterController.orderManagerRegister)
+router.post('/register/messenger', RegisterController.messengerRegister)
+module.exports = router
