f1a411ece2b2c4864ed89fd56cba9247e888f764
Mrsmithz <Dechapon.n@hotmail.com>
2021-03-20 05:24:37 +0700

    add check register route

--- server/src/controller/RegisterController.js
+++ server/src/controller/RegisterController.js
@@ -2,6 +2,7 @@ const Customer = require('../model/Customer')
 const Supervisor = require('../model/Supervisor')
 const OrderManager = require('../model/OrderManager')
 const Messenger = require('../model/Messenger')
+const Authorization = require('../model/Authorization')
 exports.customerRegister = async (req, res) => {
     var data = req.body
     var customer = new Customer(null, data.username, data.password, null, data.name, data.email, data.tel, data.address)
@@ -11,27 +12,34 @@ exports.customerRegister = async (req, res) => {
         await customer.getUserId()
         await customer.createCustomerAccount()
         await customer.getCustomerId()
-        res.send(customer)
+        res.status(201).send(customer)
     }
     catch(err){
         console.log(err)
+        res.sendStatus(400)
     }
 }
 
 exports.supervisorRegister = async (req, res) => {
     var data = req.body
-    var supervisor = new Supervisor(null, data.username, data.password, null, data.name, data.email, data.tel, data.gender, data.address)
-    try{
-        await supervisor.checkIfEmailDuplicate()
-        await supervisor.createUser()
-        await supervisor.getUserId()
-        await supervisor.createStaffAccount()
-        await supervisor.getStaffId()
-        await supervisor.createSupervisorAccount()
-        res.send(supervisor)
+    if (data.secret_key === 'doggo'){
+        var supervisor = new Supervisor(null, data.username, data.password, null, data.name, data.email, data.tel, data.gender, data.address)
+        try{
+            await supervisor.checkIfEmailDuplicate()
+            await supervisor.createUser()
+            await supervisor.getUserId()
+            await supervisor.createStaffAccount()
+            await supervisor.getStaffId()
+            await supervisor.createSupervisorAccount()
+            res.status(201).send(supervisor)
+        }
+        catch(err){
+            console.log(err)
+            res.sendStatus(400)
+        }
     }
-    catch(err){
-        console.log(err)
+    else{
+        res.sendStatus(400)
     }
 }
 exports.orderManagerRegister = async (req, res) => {
@@ -46,10 +54,11 @@ exports.orderManagerRegister = async (req, res) => {
         await order_manager.createStaffAccount()
         await order_manager.getStaffId()
         await order_manager.createOrderManagerAccount()
-        res.send(order_manager)
+        res.status(201).send(order_manager)
     }
     catch(err){
         console.log(err)
+        res.sendStatus(400)
     }
 }
 exports.messengerRegister = async (req, res) => {
@@ -58,16 +67,29 @@ exports.messengerRegister = async (req, res) => {
         data.vehicle_id, data.order_manager_id)
     try{
         await messenger.checkIfEmailDuplicate()
-        await messenger.checkVehicleId()
         await messenger.checkOrderManagerId()
         await messenger.createUser()
         await messenger.getUserId()
         await messenger.createStaffAccount()
         await messenger.getStaffId()
         await messenger.createMessengerAccount()
-        res.send(messenger)
+        res.status(201).send(messenger)
     }
     catch(err){
         console.log(err)
+        res.sendStatus(400)
+    }
+}
+
+exports.checkRegisterRoute = async (req, res, next) => {
+    try{
+        var data = req.body
+        var result = await Authorization.checkRegisterRoute(data.register_type, data.secret_key)
+        req.url = result
+        next()
+    }
+    catch (err){
+        console.log(err)
+        res.sendStatus(400)
     }
 }
