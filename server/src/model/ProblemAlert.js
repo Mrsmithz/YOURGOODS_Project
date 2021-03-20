d49d7df18216d903c9c620d8e962a3a45aae3f92
Mrsmithz <Dechapon.n@hotmail.com>
2021-03-21 05:53:40 +0700

    just import mysql file

--- server/src/model/ProblemAlert.js
+++ server/src/model/ProblemAlert.js
@@ -1,3 +1,6 @@
+const util = require('util')
+const conn = require('../database/mysql_connector')
+const mysql_query = util.promisify(conn.query).bind(conn)
 class ProblemAlert{
     constructor(id, message, problem_id){
         this.alert_id = id
