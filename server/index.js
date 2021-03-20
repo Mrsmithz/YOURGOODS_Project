935c0d06e41075445d31868f249683b5c66cb018
Mrsmithz <Dechapon.n@hotmail.com>
2021-03-21 05:55:32 +0700

    add redis store add update session management

--- server/index.js
+++ server/index.js
@@ -1,20 +1,54 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
+const redis = require('redis')
+const connectRedis = require('connect-redis')
+const session = require('express-session')
+const app = express()
 const PORT = 25800
+const RedisStore = connectRedis(session)
+const redisClient = redis.createClient({host:'redis'})
 const AuthRouter = require('./src/routes/AuthRouter')
+const ContactsRouter = require('./src/routes/ContactsRouter')
+
 app.use(cors())
 app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(`requested at ${req.url} method ${req.method}`)
     next()
 })
 
+redisClient.on('error', function (err) {
+    console.log('Could not establish a connection with redis. ' + err);
+});
+redisClient.on('connect', function (err) {
+    console.log('Connected to redis successfully');
+});
+
+app.use(session({
+    store: new RedisStore({client: redisClient}),
+    secret:'doggo_doggo',
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        secure:false,
+        httpOnly:false,
+        maxAge: 1000 * 60 * 1
+    }
+}))
 app.all('/api/*', (req, res, next) => {
-    console.log('API KEY CHECKER')
-    next()
+    console.log(req.body)
+    if (req.body.api_key == "my_doggo_name_jeff"){
+        console.log("API KEY CHECK PASS")
+        next()
+    }
+    else{
+        console.log("API KEY CHECK FAIL")
+        res.sendStatus(400)
+    }
 })
 app.use('/api/auth', AuthRouter)
+app.use('/api/contacts', ContactsRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello')
